c9e401a777a7aeb9d4938e9345ce46e2f6ad849f
serhiimgs <serhiymgs@gmail.com>
2024-09-08 14:56:38 +0300

    12

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,16 @@
+
+
+import java.util.Scanner;
+
+public class SumOfTwoNumbers {
+
+    public static void main(String[] args) {
+        int a = -3;
+        int b = 5;
+        System.out.println(a + b);
+
+    }
+    public static int sum(int a , int b) {
+        return a + b;
+    }
+}
